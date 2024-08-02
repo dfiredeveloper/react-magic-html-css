03c77e72a32bdd98719dec618ffb11d7589bc4a0
dfiredeveloper <dfiredeveloper@gmail.com>
2024-08-02 05:42:12 +0100

    snap

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-react', '@babel/preset-env'],
+    }),
+    terser(),
+  ],
+};
