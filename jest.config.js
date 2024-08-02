35b6dee30a895bc820830db077b2a78ea918b30a
dfiredeveloper <dfiredeveloper@gmail.com>
2024-08-02 06:00:29 +0100

    snap

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '\\.(css|less)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  };
+  
